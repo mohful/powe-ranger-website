555098acfac4d909e35f294eab2d6ee936a8b9c4
Mohammed Ahmed Fulwala <mohammed.fulwala@hotmail.com>
2023-08-28 11:08:19 -0400

    added vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import { ThemeProvider } from "next-themes";
 import "../css/tailwind.css";
+import { Analytics } from '@vercel/analytics/react';
 import { useState } from "react";
 
 function MyApp({ Component, pageProps }) {
@@ -7,6 +8,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider defaultTheme="dark" attribute="class">
       <Component {...pageProps} />
+      <Analytics />
     </ThemeProvider>
   );
 }
